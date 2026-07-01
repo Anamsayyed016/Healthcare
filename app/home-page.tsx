c4b73b493b61e53123e6f0b03231f46520c7cf30
anamsayyed016 <anamsayyed58@gmail.com>
2026-07-01 16:50:18 +0530

    server

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -1,10 +1,6 @@
 import SiteLayout from '@/components/site-layout';
 import HeroSection from '@/components/sections/hero';
 import ProductsPreviewSection from '@/components/sections/products-preview';
-import ServicesPreviewSection from '@/components/sections/services-preview';
-import LeadershipPreviewSection from '@/components/sections/leadership-preview';
-import WhyChoosePreviewSection from '@/components/sections/why-choose-preview';
-import ContactCtaSection from '@/components/sections/contact-cta';
 
 export default function HomePage() {
   return (
@@ -12,10 +8,6 @@ export default function HomePage() {
       <div className="flex flex-col">
         <HeroSection />
         <ProductsPreviewSection />
-        <ServicesPreviewSection />
-        <LeadershipPreviewSection />
-        <WhyChoosePreviewSection />
-        <ContactCtaSection />
       </div>
     </SiteLayout>
   );
