1d7d1aa276218c6ea29772177fc15c2cfa30cb12
anamsayyed016 <anamsayyed58@gmail.com>
2026-07-07 03:32:50 +0530

    server

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -1,13 +1,11 @@
 import SiteLayout from '@/components/site-layout';
 import HeroSection from '@/components/sections/hero';
-import ProductsPreviewSection from '@/components/sections/products-preview';
 
 export default function HomePage() {
   return (
     <SiteLayout>
       <div className="flex flex-col">
         <HeroSection />
-        <ProductsPreviewSection />
       </div>
     </SiteLayout>
   );
